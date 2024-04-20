00ba63fbf4a5203493dbaea686103f92c72f45ad
Mateus Moura Domingos <mateus.md1902@gmail.com>
2024-04-20 16:31:04 -0300

    melhorando as funcoes dos botoes

--- counter/script.js
+++ counter/script.js
@@ -2,23 +2,54 @@ const currNumber = document.getElementById("currNumber")
 const buttons = document.querySelectorAll("input")
 let num = Number(currNumber.innerHTML)
 
-buttons.forEach((value, index) => {
-    value.addEventListener("click", () => {
-        switch (index) {
-            case 0:
-                num--;
-                break;
-            case 1:
-                num = 0;
-                break;
-            case 2:
-                num++
-                break;
-            default:
-                break;
-        }
-        currNumber.textContent = num
+// function to add events to buttons
+function main() {
+    currNumber.textContent = getOnCache()
+    num = getOnCache();
+
+    // loop through the array and add functions for each button
+    buttons.forEach((value) => {
+        value.addEventListener("click", () => {
+            // get the current target button ID
+            let currentTarget = event.currentTarget.getAttribute("id");
+
+            // checks which button was clicked and adds its functions
+            switch (currentTarget) {
+                case 'btDecrease':
+                    num--;
+                    break;
+                case 'btIncrease':
+                    num++;
+                    break;
+                case 'btReset':
+                    num = 0;
+                    break;
+                default:
+                    break;
+            }
+
+            currNumber.textContent = num;
+            setOnCache(num)
+        })
     })
+}
 
+// stores the last number in the browser cache
+function setOnCache(value) {
+    if (typeof (Storage) !== "undefined") {
+        localStorage.setItem("lastNumber", value)
+    }
+}
 
-})
+// get the last number from browser cache
+function getOnCache() {
+    if (typeof (Storage) !== "undefined") {
+        if (localStorage.getItem("lastNumber")) {
+            return localStorage.getItem("lastNumber")
+        }
+    }
+
+    return 0;
+}
+
+main();
